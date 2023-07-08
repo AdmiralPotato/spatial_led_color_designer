69274fb327cf8a2887773be10345a4f85a4b2ebf
Admiral Potato <admiral@nuclearpixel.com>
2023-07-07 23:22:51 -0600

    Make header output WAY more useful for people debugging their C

--- src/composables/useProcessedOutput.js
+++ src/composables/useProcessedOutput.js
@@ -1,6 +1,8 @@
 import { ref } from 'vue';
 import useScene from '@/composables/useScene';
+import useTimeline from '@/composables/useTimeline';
 const { processOutputColors } = useScene();
+const { startFrame, endFrame, framesPerSecond } = useTimeline();
 
 const processedDownloadLink = ref(null);
 const outputName = ref('');
@@ -17,11 +19,28 @@ const formatOutputAsCHeader = (data) => {
 	const name = outputName.value.replaceAll(' ', '_');
 	const NAME = name.toLocaleUpperCase();
 	const byteStrings = data.map((n) => '0x' + n.toString(16).padStart(2, '0').toLocaleUpperCase());
-	const lines = makeSegments(byteStrings, 16).map((bytes) => `	${bytes.join(', ')}`);
+	const numFrames = endFrame.value - startFrame.value + 1;
+	const numLights = data.length / 3 / numFrames;
+	const lastLineIndex = numFrames * numLights - 1;
+	const lines = makeSegments(byteStrings, 3).map((bytes, index) => {
+		let trailingComma = ',';
+		if (index === lastLineIndex) {
+			trailingComma = '';
+		}
+		return `	${bytes.join(', ')}${trailingComma} // ${index % numLights}`;
+	});
+	const linesSegmentedByFrame = makeSegments(lines, numLights).map((lines, index) => {
+		return `	// frame index ${index}\n${lines.join('\n')}`;
+	});
 	return `#ifndef ${NAME}_H
 #define ${NAME}_H
+static const uint32_t ${name}_led_count = ${numLights};
+static const uint32_t ${name}_frame_size = ${numLights * 3};
+static const uint32_t ${name}_frame_count = ${numFrames};
+static const uint32_t ${name}_frame_rate = ${framesPerSecond.value};
+
 static const uint8_t ${name}[] = {
-${lines.join(',\n')}
+${linesSegmentedByFrame.join('\n\n')}
 };
 
 #endif /* ${NAME}_H */
@@ -55,6 +74,7 @@ export default () => {
 		processOutput: () => {
 			const dataArray = processOutputColors();
 			const cHeaderText = formatOutputAsCHeader(dataArray);
+			console.log('cHeaderText', cHeaderText);
 			processedDownloadLink.value = prepareDownload(cHeaderText, outputName.value + '.h');
 		},
 	};
